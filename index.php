edbf458f0a8bab4899620cc33f77e85a10622da3
shimantoneer <poddernakla@gmail.com>
2018-06-05 06:10:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.ratebeer.com/beer/ravenna-american-table-beer/437767/");
 die();
 ?>
